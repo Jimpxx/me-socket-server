41ec5533e3115cf9ca247116c3a9fae7c43bcff8
Jimmy Andersson <jimmyandersson89@gmail.com>
2019-10-06 21:22:36 +0200

    Initial init, ready to upload to github

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8300;
+
+const server = app.listen(port, function() {
+    console.log(`server running on port ${port}`);
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log(socket.id);
+    socket.broadcast.emit('user-connected', { message: '' });
+    socket.on('disconnect', function(data) {
+        console.log('user disconnected');
+        socket.broadcast.emit('user-disconnected', { message: '' });
+    });
+
+    // socket.on('response', function(data) {
+    //     console.log(data);
+    // });
+
+    socket.on('SEND_MESSAGE', function(data) {
+        io.emit('MESSAGE', data);
+    });
+});
