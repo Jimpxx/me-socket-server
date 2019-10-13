fe38fefee559cdbea22b5055752d0f00a9dc224e
Jimmy Andersson <jimmyandersson89@gmail.com>
2019-10-13 11:10:03 +0100

    Added mongodb and the ability for user to save and load chat

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+// const mongo = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const MongoDB = require('./db/mongodb');
+
+// const dsn = process.env.DSN || 'mongodb://localhost:27017';
 
 const app = express();
 
@@ -6,25 +11,45 @@ const port = 8300;
 
 const server = app.listen(port, function() {
     console.log(`server running on port ${port}`);
+    // console.log(`DSN is: ${dsn}`);
 });
 
 const io = require('socket.io')(server);
 
-io.origins(['https://jimmyandersson.me:443']);
+MongoDB.connectToServer(async function(err, client) {
+    if (err) throw err;
 
-io.on('connection', function(socket) {
-    console.log(socket.id);
-    socket.broadcast.emit('user-connected', { message: '' });
-    socket.on('disconnect', function(data) {
-        console.log('user disconnected');
-        socket.broadcast.emit('user-disconnected', { message: '' });
-    });
+    var db = MongoDB.getDb();
 
-    // socket.on('response', function(data) {
-    //     console.log(data);
-    // });
+    // Uncomment when deploying
+    io.origins(['https://jimmyandersson.me:443']);
 
-    socket.on('SEND_MESSAGE', function(data) {
-        io.emit('MESSAGE', data);
+    io.on('connection', function(socket) {
+        console.log(socket.id);
+        socket.broadcast.emit('user-connected', { message: '' });
+        socket.on('disconnect', function(data) {
+            console.log('user disconnected');
+            socket.broadcast.emit('user-disconnected', { message: '' });
+        });
+
+        socket.on('SAVE_CHAT', data => {
+            db.collection('texts').insertMany(data, (err, r) => {
+                if (err) throw err;
+            });
+        });
+
+        socket.on('LOAD_CHAT', data => {
+            db.collection('texts')
+                .find()
+                .toArray((err, res) => {
+                    // if (err) throw err;
+                    console.log(res);
+                    io.emit('LOAD_RESPONSE', res);
+                });
+        });
+
+        socket.on('SEND_MESSAGE', function(data) {
+            io.emit('MESSAGE', data);
+        });
     });
 });
